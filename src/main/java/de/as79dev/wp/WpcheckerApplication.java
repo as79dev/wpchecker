2ad7109eecc61fd0e530c8921ebf0816fbcb1cc5
as79 <golumn@gmx.de>
2017-04-21 22:42:53 +0200

    intialer commit

---
+++ src/main/java/de/as79dev/wp/WpcheckerApplication.java
@@ -0,0 +1,12 @@
+package de.as79dev.wp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WpcheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WpcheckerApplication.class, args);
+	}
+}
---
+++ src/test/java/de/as79dev/wp/WpcheckerApplicationTests.java
@@ -0,0 +1,16 @@
+package de.as79dev.wp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WpcheckerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
